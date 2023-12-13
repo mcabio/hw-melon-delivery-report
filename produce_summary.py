65748269aa333b964ab083109c63d3b53b43bf61
Meryl Lou Cabio <meryllou7@gmail.com>
2023-12-13 11:52:33 -0600

    Completed day 1, 2, and 3 functions

--- produce_summary.py
+++ produce_summary.py
@@ -11,23 +11,6 @@
 #     print(f"Delivered {count} {melon}s for total of ${amount}")
 # the_file.close()
 
-day_one_deliveries = open("um-deliveries-day-1.txt")
-
-def day_one(day_one_deliveries):
-    for line in day_one_deliveries:
-        line = line.rstrip()
-        words = line.split('|')
-
-        melon = words[0]
-        count = words[1]
-        amount = words[2]
-
-        print(f"Delivered {count} {melon}s for total of ${amount}")
-
-# day_one_deliveries.close()
-        
-day_one(day_one_deliveries)
-
 # print("Day 2")
 # the_file = open("um-deliveries-day-2.txt")
 # for line in the_file:
@@ -41,7 +24,6 @@ day_one(day_one_deliveries)
 #     print(f"Delivered {count} {melon}s for total of ${amount}")
 # the_file.close()
 
-
 # print("Day 3")
 # the_file = open("um-deliveries-day-3.txt")
 # for line in the_file:
@@ -54,3 +36,61 @@ day_one(day_one_deliveries)
 
 #     print(f"Delivered {count} {melon}s for total of ${amount}")
 # the_file.close()
+
+day_one_deliveries = open("um-deliveries-day-1.txt")
+day_two_deliveries = open("um-deliveries-day-2.txt")
+day_three_deliveries = open("um-deliveries-day-3.txt")
+
+# deliveries
+
+# print(deliveries)
+
+def day_one(day_one_deliveries):
+    for line in day_one_deliveries:
+        line = line.rstrip()
+        words = line.split('|')
+
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
+
+        print(f"Delivered {count} {melon}s for total of ${amount}")
+        
+day_one(day_one_deliveries)
+
+def day_two(day_two_deliveries):
+    for line in day_two_deliveries:
+        line = line.rstrip()
+        words = line.split('|')
+
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
+
+        print(f"Delivered {count} {melon}s for total of ${amount}")
+        
+day_two(day_two_deliveries)
+
+def day_three(day_three_deliveries):
+    for line in day_three_deliveries:
+        line = line.rstrip()
+        words = line.split('|')
+
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
+
+        print(f"Delivered {count} {melon}s for total of ${amount}")
+        
+day_three(day_three_deliveries)
+
+def day_three(day_three_deliveries):
+    for line in day_three_deliveries:
+        line = line.rstrip()
+        words = line.split('|')
+
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
+
+        print(f"Delivered {count} {melon}s for total of ${amount}")
